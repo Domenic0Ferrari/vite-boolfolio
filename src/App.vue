3cc32d227aa1733b5cb491d64b7096cd94c9501a
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-07-14 17:23:26 +0200

    add v-for in li and use api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script>
+import ProjectCard from './components/ProjectCard.vue';
+export default {
+    components: {
+        ProjectCard,
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <ProjectCard />
+</template>
 
 <style lang="scss">
 @import "bootstrap/scss/bootstrap";
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,9 +1,23 @@
 <script>
+import axios from 'axios';
+
 export default {
+    data() {
+        return {
+            arrProjects: [],
+        }
+    },
+    created() {
+        axios.get('http://localhost:8000/api/projects').then(response => this.arrProjects = response.data.data)
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <ul>
+        <li v-for="project in arrProjects" :key="project.id">{{ project.title }}</li>
+    </ul>
+</template>
         
 
 <style lang="scss" scoped></style>
