21c2e486d6d3f1a4d048f17bb4e92acc26d9e49a
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-07-17 17:03:05 +0200

    add 'author' in cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -22,6 +22,7 @@ export default {
         <img :src="getImageUrl(objProject.image)" class="card-img-top" :alt="objProject.title">
         <div class="card-body d-flex flex-column align-items-center g-3 justify-content-end">
             <h5 class="card-title">{{ objProject.title }}</h5>
+            <h6>{{ objProject.author }}</h6>
             <p class="card-text">text</p>
             <router-link :to="{ name: 'projects.show', params: { slug: objProject.slug } }" class="btn btn-primary">
                 Show
