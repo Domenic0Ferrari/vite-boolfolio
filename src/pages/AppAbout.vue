8b9cb9861a9797a53b3df923481be43b68f4dd80
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-07-18 15:01:38 +0200

    add appAbout.vue in pages

---
+++ src/pages/AppAbout.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <h1 class="text-center">About Us</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptatum molestias? Sit animi ut velit alias
+        illum quos laudantium distinctio provident voluptates commodi consectetur eius voluptatem, obcaecati, non facere
+        unde!
+        Accusamus sed libero aliquam amet ut consectetur, cupiditate aperiam, quidem in ipsum praesentium tempora officiis.
+        Reprehenderit animi voluptatibus quisquam iure eum rerum omnis, sequi commodi earum modi dignissimos quod eligendi.
+        Maxime cum voluptate eaque facere laudantium pariatur deserunt. Quas in dicta et reiciendis temporibus error
+        perferendis ipsum praesentium. Tempore voluptas quasi eveniet qui sit iste harum ut dolore iusto. Assumenda.
+        Enim tempora voluptatibus maxime, omnis nemo numquam earum culpa quaerat tempore doloremque voluptate velit eius
+        quidem fugiat accusantium, illo quos reprehenderit. Commodi totam eligendi earum ipsam deleniti repellendus. Ex,
+        similique.
+        Doloribus aliquam, laboriosam porro reprehenderit molestias corrupti accusamus atque doloremque neque natus sapiente
+        quod non culpa odio odit veritatis! Ad hic animi iste officia expedita non dolores veniam sit magnam.
+        Odit voluptatem earum hic aperiam voluptates distinctio nam delectus aspernatur officia eius illum asperiores vel
+        qui pariatur expedita iste nemo iure excepturi, obcaecati commodi ad. Provident saepe dicta autem necessitatibus.
+    </p>
+</template>
+
+<style lang="scss" scoped></style>
